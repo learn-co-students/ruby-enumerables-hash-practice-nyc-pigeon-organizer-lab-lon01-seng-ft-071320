bf59804d3226bb46055ee78882cfdd0387a4147b
Alex Marshall <alex.k.marshall83@gmail.com>
2020-03-24 12:34:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
+# data is hash of hash of arrays
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pp data
+  
+  result = {}
+  
+  data.each_pair do |key, value|
+    new_attribute = key
+    puts new_attribute
+    value.each_pair do |key, value|
+      new_value = key.to_s
+      puts new_value
+      
+      value.each do |name|
+        if result[name]
+          if result[name][new_attribute]
+            result[name][new_attribute] << new_value
+          else 
+            result[name][new_attribute] = [new_value]
+          end        
+        else
+          result[name]= {}
+          result[name][new_attribute] = [new_value]
+        end
+        pp result
+      end
+        
+      #value.each do |el|
+      #  puts "#{el} => #{new_attribute} => #{new_value}"
+      #end
+    end
+  end
+  
+  result
 end
+
+
+
+
